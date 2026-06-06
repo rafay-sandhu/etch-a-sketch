e4da94589ed05004801c87f87e04d3568c2b11c2
rafay-sandhu <88657320+rafay-sandhu@users.noreply.github.com>
2026-06-07 02:53:37 +0500

    Add button to ask user for number of grid boxes. Change event listener for color changing from hovering to mouse left-click while hovering. Randomize color of each grid box to give a more beautiful sketch.

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
 const container = document.querySelector(".container");
+const btn = document.querySelector("button");
 
-for (let i = 0; i < 16; i++) {
-    const gridbox = document.createElement("div");
-    gridbox.classList.add("gridbox");   
-    container.appendChild(gridbox);
+function createGrid(grid_box_number) {
 
-    for (let j = 0; j < 16; j++) {
-        const gridsubbox = document.createElement("div");
-        gridsubbox.classList.add("gridsubbox");
-        
-        gridsubbox.addEventListener("mouseenter", () => {
-            gridsubbox.classList.add("hovercolor");
-        });
-        
-        gridbox.appendChild(gridsubbox);
+    container.innerHTML = "";
+
+    for (let i = 0; i < grid_box_number; i++) {
+        const gridbox = document.createElement("div");
+        gridbox.classList.add("gridbox");   
+        container.appendChild(gridbox);
+
+        for (let j = 0; j < grid_box_number; j++) {
+            const gridsubbox = document.createElement("div");
+            gridsubbox.classList.add("gridsubbox");
+
+            gridsubbox.addEventListener("mouseenter", (e) => {
+                if (e.buttons == 1) {
+                    gridsubbox.classList.add("hovercolor");
+                    const r = Math.floor(Math.random() * 256);
+                    const g = Math.floor(Math.random() * 256);
+                    const b = Math.floor(Math.random() * 256);
+                    gridsubbox.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+                }
+            });
+            
+            gridbox.appendChild(gridsubbox);
+        }
     }
 }
+
+createGrid(16);
+
+btn.addEventListener("click", function() {
+    let input_value;
+    while(true) {
+        let raw_input = prompt("Please enter the number of boxes for the grid:");
+        
+        if (raw_input === null) {
+            return;
+        }
+
+        input_value = Number(raw_input);
+        if (!isNaN(input_value) && input_value > 0 && input_value < 101) {
+            break;
+        }
+    }
+    createGrid(input_value);
+})
